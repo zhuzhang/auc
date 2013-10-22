c0ade6bb231f2d99dd3ebbca956a37171fc27dd5
Zhu Zhang <zebra.zhu@gmail.com>
2013-10-22 23:49:02 +0800

    Init commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Auction::Application.config.secret_key_base = '16bd6d6edec64259d5a8631ef4b5be65a0af976f80cb7a8d1e20116c34cda1b7e867f1d642baaef5d88d720e5045516df41e2450294e9d6628af9992b6b66f46'
