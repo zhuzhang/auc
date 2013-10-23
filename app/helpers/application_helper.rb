12e5d22aab2dbdcb2ca503944c772043f6367456
Zhu Zhang <zebra.zhu@gmail.com>
2013-10-23 22:14:42 +0800

    Add bootstrap-sass gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def bootstrap_class_for flash_type
+        {sucess: 'alert-success', error: 'alert-error',
+         alert: 'alert-block', notice: 'alert-notice'}[flash_type] || flash_type.to_s
+    end
 end
